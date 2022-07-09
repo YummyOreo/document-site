c3220abcdaf645cfcc618ddc203c103705ca79e0
YummyOreo <bobgim20@gmail.com>
2022-07-09 10:52:33 -0500

    implements firebase and some headackes

--- backend/index.ts
+++ backend/index.ts
@@ -1,20 +1,13 @@
 import express from "express";
-import { Paths } from "./src/ts/constants/paths";
 const app = express();
 const port = 3000;
 
 import EndpointController from "./src/ts/controller/endpoint-controller";
 
-const root = { root: Paths["root"] };
-console.log(Paths["root"]);
-
-const htmlPath = Paths["pages"];
-const htmlFiles = { index: htmlPath + "/index.html" };
-
 const endpoint: EndpointController = new EndpointController(app);
 
-app.get("/", (req, res) => {
-  res.sendFile(htmlFiles["index"], root);
+app.get("/api", (req, res) => {
+  res.send("hello");
 });
 
 app.listen(port, () => {
--- backend/src/ts/constants/paths.ts
+++
@@ -1,6 +0,0 @@
-const root = __dirname.replace("backend\\src\\ts\\constants", "");
-
-export const Paths = {
-  root: root,
-  pages: "./frontend/static/html/pages",
-};
