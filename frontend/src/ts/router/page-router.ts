c2a164180014322855c684074c8098e8ec74fdad
YummyOreo <bobgim20@gmail.com>
2022-07-12 14:52:05 -0500

    adds css loading

--- frontend/src/ts/router/page-router.ts
+++ frontend/src/ts/router/page-router.ts
@@ -17,13 +17,19 @@ export default async function getPage(): Promise<void> {
   displayPage(error);
 }
 
-async function displayPage(page) {
+async function displayPage(page: Page) {
   $(".main").load(`/static/pages/${page.html}`, async () => {
     $(document).prop("title", page.name);
+    $("head").append(
+      $('<link rel="stylesheet" type="text/css" />').attr(
+        "href",
+        `/src/css/${page.css}`
+      )
+    );
     await runPage(page);
   });
 }
 
-async function runPage(page: any) {
+async function runPage(page: Page) {
   await page.run();
 }
