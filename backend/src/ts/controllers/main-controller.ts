739e6cd432ffa46edf3187ec3b47e82970e831f1
YummyOreo <bobgim20@gmail.com>
2022-07-17 20:28:27 -0500

    cleans up

--- backend/src/ts/controllers/main-controller.ts
+++ backend/src/ts/controllers/main-controller.ts
@@ -1,18 +1,8 @@
 import { Request, Response } from "express";
-import { getCollection } from "../db/collections/records";
 import { client, connect } from "../db/db";
 
 export async function hello(req: Request, res: Response) {
-  console.log("hello");
-
-  await connect().then(() => {
-    console.log("test");
-
-    getCollection().insertOne({ a: 1 });
-    getCollection().find({}).toArray();
-
-    console.log(getCollection());
-
+  await connect().then(async () => {
     setTimeout(() => {
       client.close();
     }, 1500);
