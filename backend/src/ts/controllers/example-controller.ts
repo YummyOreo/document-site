1d9cd1d4cfe763785c272d07f26951f218b854f3
YummyOreo <bobgim20@gmail.com>
2022-07-18 20:46:01 -0500

    refactors some of the dal

--- backend/src/ts/controllers/example-controller.ts
+++ backend/src/ts/controllers/example-controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { getDocuments } from "../dal/example";
 
 export async function getExample(req: Request, res: Response) {
-  console.log(await Promise.resolve(getDocuments()));
+  console.log(await getDocuments());
 
   res.status(202).send({ hello: "ello" });
 }
--- backend/src/ts/dal/example.ts
+++ backend/src/ts/dal/example.ts
@@ -2,5 +2,5 @@ import { Document, WithId } from "mongodb";
 import { getCollection } from "../db/collections/records";
 
 export async function getDocuments(): Promise<WithId<Document>[]> {
-  return getCollection().find({}).toArray();
+  return await getCollection().find({}).toArray();
 }
