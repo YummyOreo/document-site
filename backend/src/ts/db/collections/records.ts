2d6dc9d46000c529fe4e56f851b197d274a6695c
YummyOreo <bobgim20@gmail.com>
2022-08-13 17:30:16 -0500

    adds basic making of documents
    - still need to make custom ids for them
    - give back the id

--- backend/src/ts/db/collections/records.ts
+++ backend/src/ts/db/collections/records.ts
@@ -1,6 +1,8 @@
 import { Collection, Document } from "mongodb";
 import { db } from "../db";
 
+// Records are documents
+
 const COLLECTION_NAME = "records";
 
 export function getCollection(): Collection<Document> {
--- backend/src/ts/routes/document.ts
+++ backend/src/ts/routes/document.ts
@@ -1,4 +1,5 @@
 import * as express from "express";
+import { getCollection } from "../db/collections/records";
 
 export const router = express.Router();
 
@@ -9,5 +10,27 @@ export const router = express.Router();
   - uses Markdown
 */
 router.post("/", async (req: express.Request, res: express.Response) => {
+  if (req.headers["content-type"] != "text/plain") {
+    res
+      .status(400)
+      .send('Please set the "content-type" header to "text/plain"');
+    return;
+  }
+
+  if (!("title" in req.query)) {
+    res.status(400).send("Plese provide a title in the query params");
+    return;
+  }
+
+  if (req.body == "") {
+    res.status(400).send("Please provide a body");
+    return;
+  }
+
+  await getCollection().insertOne({
+    title: req.query["title"],
+    body: req.body,
+  });
+
   res.status(200).send("Document made: " + req.body);
 });
