39a48b78c84246db2f0974e0804e0b4035d609ea
YummyOreo <bobgim20@gmail.com>
2022-08-23 17:40:04 -0500

    adds name popup

--- frontend/src/ts/pages/doc-make.ts
+++ frontend/src/ts/pages/doc-make.ts
@@ -1,4 +1,6 @@
-import { PageDefault } from "../types/classes";
+import { closeOnClick } from "../popup/common-popups";
+import { PageDefault, PopupDefault } from "../types/classes";
+import * as popupController from "../popup/popup-controller";
 
 export const urls = ["/make"];
 
@@ -29,9 +31,71 @@ export class Page extends PageDefault {
   async run() {
     super.run();
 
+    $("#doc-title").text("Unamed");
+
     $("#body").on("blur", (e) => {
-      const target: HTMLTextAreaElement | any = e.target;
-      this.body = target.value;
+      this.body = $(e.target).val().toString();
+      console.log(this.body);
     });
+
+    $("#doc-title").on("click", async () => {
+      const namePopupId = await this.makeNamePopup();
+      popupController.showPopup(namePopupId);
+    });
+
+    const namePopupId = await this.makeNamePopup();
+    popupController.showPopup(namePopupId);
+  }
+
+  async makeNamePopup() {
+    const popup = new PopupDefault(
+      "50%",
+      "80%",
+      "var(--background-color-3)",
+      "docName.html",
+      "docName.css"
+    );
+    popup.makeFunc = (popup: PopupDefault) => {
+      closeOnClick(popup.id);
+
+      if (this.title != "") {
+        const elm: any = document.getElementById("doc-input-title");
+        elm.value = this.title;
+      }
+
+      $(".doc-name-submit").on("click", () => {
+        popupController.hidePopup(popup.id);
+
+        setTimeout(() => {
+          popupController.deletePopup(popup.id);
+        }, 300);
+      });
+
+      $(".doc-name-popup").on("keypress", function (e) {
+        if (e.which == 13) {
+          popupController.hidePopup(popup.id);
+
+          setTimeout(() => {
+            popupController.deletePopup(popup.id);
+          }, 300);
+        }
+      });
+    };
+
+    popup.hideFunc = (popup: any) => {
+      if (this.title == "") {
+        this.title = "Unamed";
+      } else {
+        const elm: any = document.getElementById("doc-input-title");
+        this.title = elm.value;
+      }
+
+      $("#doc-title").text(this.title);
+      console.log(this.title);
+    };
+
+    const id = popupController.makePopup(popup);
+
+    return id;
   }
 }
