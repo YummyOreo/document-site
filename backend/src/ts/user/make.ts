6fa1d982842c4f1ed8010400ae5f688621415beb
YummyOreo <bobgim20@gmail.com>
2022-08-28 13:21:55 -0500

    clean up code

--- backend/src/ts/user/make.ts
+++ backend/src/ts/user/make.ts
@@ -1,10 +1,30 @@
 import * as express from "express";
 import { getCollection } from "../db/collections/users";
 import { ID, SECRET, REDIRECT } from "../env/auth";
-import DiscordOauth2 = require("discord-oauth2");
 import { CLIENT_URL } from "../env/other";
 
+import DiscordOauth2 = require("discord-oauth2");
+
 export async function make(req: express.Request, res: express.Response) {
+  const [user, token] = await Oauth(req);
+
+  const userScheme = {
+    id: user["id"],
+    name: user["username"],
+    discriminator: user["discriminator"],
+    token: token["access_token"],
+  };
+
+  if ((await getCollection().findOne({ id: user["id"] })) != null) {
+    getCollection().findOneAndReplace({ id: user["id"] }, userScheme);
+  } else {
+    getCollection().insertOne(userScheme);
+  }
+
+  res.status(304).redirect(`${CLIENT_URL}?token=${token["access_token"]}`);
+}
+
+async function Oauth(req) {
   const oauth = new DiscordOauth2({
     clientId: ID,
     clientSecret: SECRET,
@@ -21,47 +41,5 @@ export async function make(req: express.Request, res: express.Response) {
     })
     .catch(console.log);
 
-  const user = await oauth.getUser(token["access_token"]);
-
-  if (await checkUser(user, token)) {
-    return res
-      .status(304)
-      .redirect(`${CLIENT_URL}?token=${token["access_token"]}`);
-  }
-
-  updateUser(user, token, res);
-}
-
-async function checkUser(user, token): Promise<boolean> {
-  if ((await getCollection().findOne({ id: user["id"] })) != null) {
-    getCollection()
-      .findOneAndReplace(
-        { id: user["id"] },
-        {
-          id: user["id"],
-          name: user["username"],
-          discriminator: user["discriminator"],
-          token: token["access_token"],
-        }
-      )
-      .catch(console.log);
-
-    return true;
-  }
-  return false;
-}
-
-async function updateUser(user, token, res) {
-  getCollection()
-    .insertOne({
-      id: user["id"],
-      name: user["username"],
-      discriminator: user["discriminator"],
-      token: token["access_token"],
-    })
-    .catch(console.log);
-
-  return res
-    .status(304)
-    .redirect(`${CLIENT_URL}?token=${token["access_token"]}`);
+  return [await oauth.getUser(token["access_token"]), token];
 }
