f7d920cac3320fd0134f2ac6851d1489921d6dda
YummyOreo <bobgim20@gmail.com>
2022-09-17 11:34:18 -0500

    pushes to db

--- bot/index.ts
+++ bot/index.ts
@@ -1,8 +1,16 @@
-import { AuditLogEvent, Client, GatewayIntentBits } from "discord.js";
+import {
+  AuditLogEvent,
+  CacheType,
+  Client,
+  GatewayIntentBits,
+  Interaction,
+} from "discord.js";
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 import * as dotenv from "dotenv";
 dotenv.config();
+
+import { Db, MongoClient } from "mongodb";
 
 export function getEnv(name: string): string {
   const envVar = process.env[name];
@@ -38,57 +46,60 @@ export function getEnv(name: string): string {
 //   }
 // })();
 
+const clientDb = new MongoClient(getEnv("MONGODB_URL"));
+let db: Db = clientDb.db(getEnv("DB_NAME"));
+
 client.on("ready", (client) => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
+
+async function getRoles(interaction: Interaction<CacheType>) {
+  const roles = interaction.guild?.roles.cache.map((role) => role);
+
+  const above = "826150208329089106";
+  const pos = await interaction.guild?.roles.fetch(above).then((role) => {
+    return role?.position;
+  });
+  if (pos == undefined) return;
+
+  const filteredRoles: any[] = [];
+  roles?.forEach(async (role) => {
+    if (role.position >= pos) {
+      filteredRoles.push({
+        role,
+        members: role.members,
+      });
+    }
+  });
+
+  const converted = [];
+
+  for (const i in filteredRoles) {
+    const role = filteredRoles[i];
+
+    const color =
+      role.role.color.toString(16) == 0
+        ? "99aab5"
+        : role.role.color.toString(16);
+
+    const id = role.role.id;
+    const name = role.role.name;
+    const position = role.role.rawPosition;
+
+    const members = role.members.map((value: any, key: any) => key);
+
+    converted.push({ id, name, position, members, color });
+  }
+  db.collection("roles").deleteMany({});
+  db.collection("roles").insertMany(converted);
+  console.log(await db.collection("roles").find({}).toArray());
+}
 
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
   if (interaction.commandName === "ping") {
-    const roles = interaction.guild?.roles.cache.map((role) => role);
-
-    const above = "826150208329089106";
-    const pos = await interaction.guild?.roles.fetch(above).then((role) => {
-      return role?.position;
-    });
-    if (pos == undefined) return;
-
-    const filteredRoles: any[] = [];
-    roles?.forEach(async (role) => {
-      if (role.position >= pos) {
-        filteredRoles.push({ role, members: role?.members });
-      }
-    });
-
-    for (const i in filteredRoles) {
-      console.log({
-        role: filteredRoles[i].role,
-        members: filteredRoles[i].members,
-      });
-    }
-
-    let converted = {};
-
-    for (const i in filteredRoles) {
-      const role = filteredRoles[i];
-
-      const color =
-        role.role.color.toString(16) == 0
-          ? "99aab5"
-          : role.role.color.toString(16);
-
-      const id = role.role.id;
-      const name = role.role.name;
-      const position = role.role.rawPosition;
-
-      const members = role.members.map((value: any, key: any) => key);
-
-      console.log({ color, id, name, position, members });
-    }
-
-    // colors have to be converted to hex ie 15844367 -> 0xF1C40F (then remove the 0x)
-
+    await getRoles(interaction);
     await interaction.reply("Pong!");
   }
 });
