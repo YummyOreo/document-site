36dc8f64451e2e236558331859e9e9118251d096
YummyOreo <bobgim20@gmail.com>
2022-10-06 21:26:14 -0500

    filters the list request to return everything but the users if the user in not a admin

--- backend/src/ts/groups/list.ts
+++ backend/src/ts/groups/list.ts
@@ -1,8 +1,25 @@
 import * as express from "express";
+import { checkPerms } from "../config/get";
 import { getCollection } from "../db/collections/groups";
 
 export async function getAll(_: any, res: express.Response) {
   const groups = await getCollection().find({}).toArray();
 
-  res.status(202).send({ groups });
+  res.status(202).send({ groups: filter(groups) });
+}
+
+function filter(groups: any[]) {
+  if (checkPerms("admin")) {
+    return groups;
+  }
+
+  const newGroups = [];
+
+  for (const i in groups) {
+    const group = groups[i];
+    const newGroup = group;
+    newGroup["users"] = undefined;
+    newGroups.push(newGroup);
+  }
+  return newGroups;
 }
