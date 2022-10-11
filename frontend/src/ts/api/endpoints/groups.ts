db7697b65c386bba74b258105e91b7af02ea2e96
YummyOreo <bobgim20@gmail.com>
2022-10-11 16:46:17 -0500

    adds a function to make the request to add a group

--- frontend/src/ts/api/endpoints/groups.ts
+++ frontend/src/ts/api/endpoints/groups.ts
@@ -1,4 +1,5 @@
 import { auth } from "../../store";
+import { Group } from "../../types/FrontendTypes";
 import * as apiUtils from "../utils";
 
 const BASE = "/groups";
@@ -9,5 +10,22 @@ export async function getGroups() {
     `${apiUtils.api["url"]}${BASE}/getAll`,
     undefined,
     auth.token
+  );
+}
+
+export async function makeGroup(group: Group) {
+  return await apiUtils.MakeRequest(
+    "POST",
+    `${apiUtils.api["url"]}${BASE}/`,
+    {
+      users: group.users,
+    },
+    auth.token,
+    {
+      name: group.name,
+      color: group.color,
+      position: group.position.toString(),
+    },
+    "application/json"
   );
 }
